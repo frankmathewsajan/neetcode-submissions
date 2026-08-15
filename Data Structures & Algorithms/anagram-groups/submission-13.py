16ae84bdb1ada9dfc302c1f0f36338578e914b3a
Frank Mathew Sajan <145902617+frankmathewsajan@users.noreply.github.com>
2026-08-15 22:36:57 +0530

    Add: anagram-groups - submission-13

---
+++ Data Structures & Algorithms/anagram-groups/submission-13.py
@@ -0,0 +1,11 @@
+from collections import defaultdict
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        seen = defaultdict(list)
+        for s in strs:
+            temp = [0]*26
+            for c in s:
+                temp[ord(c) - ord('a')] += 1
+            seen[bytes(temp)].append(s) 
+        return list(seen.values())
