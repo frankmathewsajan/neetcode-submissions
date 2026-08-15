2cd84427a1942993da48625a484b51cd326acab4
Frank Mathew Sajan <145902617+frankmathewsajan@users.noreply.github.com>
2026-08-15 22:31:00 +0530

    Add: anagram-groups - submission-7

---
+++ Data Structures & Algorithms/anagram-groups/submission-7.py
@@ -0,0 +1,11 @@
+from collections import defaultdict
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        seen = defaultdict(list)
+        for s in strs:
+            temp = [0]*27
+            for c in s:
+                temp[ord(c) - ord('a')] += 1
+            seen[tuple(temp)].append(s)
+        return list(seen.values())
